2f2fc414c239682bb8f392a368edc4b7cd08875d
MOMO Adrien Nickson <madriennickson@gmail.com>
2024-01-02 20:46:09 +0100

    feat: Change nonce.next_path from charfield to textfield

--- src/django_keycloak/models.py
+++ src/django_keycloak/models.py
@@ -232,7 +232,7 @@ class Nonce(models.Model):
 
     state = models.UUIDField(default=uuid.uuid4, unique=True)
     redirect_uri = models.CharField(max_length=255)
-    next_path = models.CharField(max_length=255, null=True)
+    next_path = models.TextField(null=True)
 
 
 class ExchangedToken(TokenModelAbstract):
